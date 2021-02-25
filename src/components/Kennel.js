2a8c735d8fbfb8236baa39a97d65b2b08cfccd35
Ccourter11 <courtercalvin@yahoo.com>
2021-02-24 19:26:37 -0600

    added in jisies beautiful code

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -11,26 +11,89 @@ import "./employee/EmployeeCard";
 
 // Functional Component
 // the single line is whatever gets returned in the parenthesis
-export const Kennel = () => (
-    <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
 
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
 
-        <h2>Animals</h2>
-        <article className="animals">
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-        </article>
-    </>
-)
+    export const Kennel = () => {
+        const kennel = {
+          name: "Nashville Kennels: #1 in Davidson County",
+          locations: [
+            {
+              name: "Nashville North",
+              address: "500 Puppy Way"
+            }
+          ]
+        }
+      
+        const animals = [
+          {
+            "name": "Doodles",
+            "breed": "Poodle",
+            "locationId": 1,
+            "customerId": 3,
+            "id": 1
+          },
+          {
+            "id": 2,
+            "name": "Kelvin",
+            "breed": "Bulldog",
+            "customerId": 3,
+            "locationId": 2,
+          },
+          {
+            "name": "Midnight",
+            "breed": "Bulldog",
+            "locationId": 2,
+            "customerId": 3,
+            "id": 3
+          },
+          {
+            "name": "Chowder",
+            "breed": "Pomeranian",
+            "locationId": 1,
+            "customerId": 3,
+            "id": 4
+          },
+          {
+            "name": "Nimbus",
+            "breed": "Bulldog",
+            "locationId": 1,
+            "customerId": 3,
+            "id": 5
+          }
+        ]
+      
+        const koopa = {
+          name: "Koopa",
+          breed: "Sulcata tortoise"
+        }
+      
+        // debugger
+      
+        return (
+          <>
+            {console.log("kennel object", kennel.locations)}
+            <h2>{kennel.name}</h2>
+            <small>Loving care when you're not there.</small>
+            <address>
+              <div>Visit Us at the {kennel.locations[0].name} Location</div>
+              <div>{kennel.locations[0].address}</div>
+            </address>
+            <article className="animals">
+              {
+                animals.map(animal => {
+                  return <AnimalCard key={animal.id} animal={animal} />
+                })
+              }
+            </article>
+      
+            <AnimalCard animal={koopa} />
+          </>
+        )
+      }
 
 export const Customer = () => (
+
+      
     <>
         
         <h2>Customers</h2>
