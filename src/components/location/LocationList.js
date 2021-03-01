44d02e5aa07c26dd93e618cf46e4275db9ed249d
Ccourter11 <courtercalvin@yahoo.com>
2021-03-01 13:31:53 -0600

    location list/card

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,3 +1,36 @@
+// import React, { useEffect, useContext } from "react"
+// import { LocationCard } from "./LocationCard";
+// import "./Location.css"
+// // To start, you need to import the context object you created in the provider component so that the Context hook can access the objects it exposes
+// import { LocationContext } from "./LocationProvider";
+
+// export const LocationList = () => {
+//   // This state changes when `getLocations()` is invoked below
+//   const { locations, getLocations } = useContext(LocationContext)
+
+
+//    // The useEffect hook allows the component to reach out into the world for anything that cannot be handled during render. In this case, it is the API call for the locations.
+//    useEffect(() => {
+//     console.log("LocationList: useEffect - getLocations")
+//     getLocations()
+//   }, [])
+
+
+//   return (
+//   <>
+//     <h4>Locations</h4>
+//     <article className="locations">
+//         {/* Use the .map() array method to iterate the array of animals and generate HTML for each one by invoking the LocationCard component function. */}
+//       {locations.map(locationObject => {
+//         return <LocationCard key={locationObject.id} locationProps={locationObject} />
+//       })
+//       }
+//        {/* even though it looks like you are specifying an HTML component, you are actually invoking a function. Also, the key and location arguments look like HTML attributes here, but they actually become properties on an object that gets passed as an argument */}
+//     </article>
+//   </>
+// )
+// }
+
 import React from "react"
 
 export const LocationList = () => (
