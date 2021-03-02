b2363aafa01872fa38d05cb14de60371f72289a7
Ccourter11 <courtercalvin@yahoo.com>
2021-03-02 10:34:11 -0600

    bug ch 10

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,16 +1,40 @@
 // My Main App Component
 
-import React from "react"
-import { NavBar } from "./nav/NavBar"
-import { ApplicationViews } from "./ApplicationViews"
-import "./Kennel.css"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews";
+import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import "./Kennel.css";
 
-export const Kennel = () => (
-    <>
-        <NavBar />
-        <ApplicationViews />
-    </>
-)
 
 // NavBar is the presentation component
 // ApplicationViews is the controller component
+
+
+export const Kennel = () => (
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("kennel_customer")) {
+          return (
+            <>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+);
